14d6cd31ad7a5fa0fecb0c3d72bfa5bafc2e63a2
John Hancock <jhancoc4@fau.edu>
2015-05-02 23:19:52 -0400

    remove unnecessary code

--- 10qcruncher/10qcruncher-core/src/main/java/com/rootser/qcruncher/examples/JaxbAtomDemo.java
+++
@@ -1,7 +0,0 @@
-package com.rootser.qcruncher.examples;
-import com.rootser.qcruncher.generated.*;
-public class JaxbAtomDemo {
-	public static void main(String[] args) {
-		//com.rootser.qcruncher.generated.
-	}
-}
