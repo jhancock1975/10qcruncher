e1d96d609263c4d1c43eaea659aabd006879e609
John Hancock <jhancoc4@fau.edu>
2015-05-05 21:30:10 -0400

    add unit test for DocRetrievalSvc

---
+++ 10qcruncher/10qcruncher-core/src/test/java/com/rootser/qcruncher/service/DocRetrievalSvcTest.java
@@ -0,0 +1,93 @@
+package com.rootser.qcruncher.service;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.google.common.collect.Lists;
+import com.rootser.qcruncher.common.AppMsg;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:XmlReaderConfig.xml" })
+@Configuration
+@PropertySource("classpath:service-tests.properties")
+public class DocRetrievalSvcTest {
+
+	@Autowired
+	private DocRetrievalSvc docSvc;
+	
+	@Value("${doc.retrieval.test.url}")
+	private String testUrlStr;
+	
+	@Value("${doc.retrieval.test.txt.url}")
+	private String testTxtUrlStr;
+	
+	private AppMsg<String> getUrlMsg(){
+		AppMsg<String> urlMsg = new AppMsg<String>();
+		urlMsg.setResult(testUrlStr);
+		return urlMsg;
+	}
+	
+	@Test
+	public void testGetHtmlDoc() {
+		AppMsg<String> urlMsg = getUrlMsg();
+		
+		AppMsg<Document> docMsg = docSvc.getHtmlDoc(urlMsg);
+		
+		boolean passCond = ! docMsg.hasErrors() &&
+				docMsg.getResult().title() != null;
+		
+		assertTrue(passCond);
+	}
+	
+	@Test
+	public void testGetHtmlDocs() {
+		@SuppressWarnings("unchecked")
+		List<AppMsg<String>> urlMsgs = Lists.newArrayList(getUrlMsg(), getUrlMsg(), getUrlMsg());
+		
+		List<AppMsg<Document>> docMsgs = docSvc.getHtmlDocs(urlMsgs);
+		
+		for (int i = 0; i < docMsgs.size(); i++){
+			boolean passCond = ! docMsgs.get(i).hasErrors() &&
+					docMsgs.get(i).getResult().title() != null;
+			assertTrue("failed to download document #" + i, passCond);
+		}
+	}
+	
+	@Test
+	public void testGetTxtDoc() {
+		AppMsg<String> urlMsg = getUrlMsg();
+		
+		AppMsg<String> docMsg = docSvc.getTxtDoc(urlMsg);
+		
+		boolean passCond = ! docMsg.hasErrors() &&
+				docMsg.getResult().contains("10qcruncher is software");
+		
+		assertTrue(passCond);
+	}
+	
+	@Test
+	public void testGetTxtDocs() {
+		@SuppressWarnings("unchecked")
+		List<AppMsg<String>> urlMsgs = Lists.newArrayList(getUrlMsg(), getUrlMsg(), getUrlMsg());
+		
+		List<AppMsg<String>> docMsgs = docSvc.getTxtDocs(urlMsgs);
+		
+		for (int i = 0; i < docMsgs.size(); i++){
+			boolean passCond =  ! docMsgs.get(i).hasErrors() &&
+					docMsgs.get(i).getResult().contains("10qcruncher is software");
+			assertTrue("failed to download document #" + i, passCond);
+		}
+	}
+}
