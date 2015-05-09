47d8575586414ab3d6287507043cd5548bb71725
John Hancock <jhancoc4@fau.edu>
2015-05-09 07:07:13 -0400

    add unit test and improve error handling

---
+++ 10qcruncher/10qcruncher-core/src/test/java/com/rootser/qcruncher/service/RawDataSvcTest.java
@@ -0,0 +1,14 @@
+package com.rootser.qcruncher.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RawDataSvcTest {
+
+	@Test
+	public void testGetRawData() {
+		fail("Not yet implemented");
+	}
+
+}
